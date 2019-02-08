9ffc8d1477ea4bfd6101f0acfead7f0b92099dd2
MarcusJones <jones.0bj3@gmail.com>
2019-02-08 20:58:43 +0100

    update

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,5 +1,15 @@
 from corral.dataset import AIDataSet
+from pathlib import Path
 
+path_sample_root = Path.cwd() / 'sample'
+assert path_sample_root.exists()
+path_data = Path.cwd() / 'sample'
+data_folder = '20181104 1200'
+
+AIDataSet(path_data, data_folder)
 def test_load():
     # ds = AIDataSet(LOCAL_PROJECT_PATH, DATASET_ID)
+    path_jpg = path_sample_root / 'jpg'
+    path_json = path_sample_root / 'json'
+    path_jpg.glob('*.jpg')
     assert True
---
+++ utilities/dataset.py
@@ -0,0 +1,22 @@
+# Standard library
+# import datetime
+# import json
+# import logging
+# import os
+# import re
+# import zipfile
+from pathlib import Path
+
+# Third party
+import numpy as np
+import pandas as pd
+
+
+class Dataset()
+    def __init__(self):
+        pass
+
+path_data_root = Path.home() / 'British_birdsong' / 'full'
+def get_input():
+
+
